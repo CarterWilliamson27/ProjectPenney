fb681813fd6c1a131695db7d1dd2f18d3c8e4d51
Carter Williamson <williamsoncarter27@gmail.com>
2025-09-19 13:51:12 -0400

    add Method 2 markdown generation

--- testing/src/buildMarkdownFile.py
+++ testing/src/buildMarkdownFile.py
@@ -17,38 +17,53 @@ def buildMarkdownFile():
     linebreak = "___"
     title = "## Project Penney Data Generation<br><sup>Carter Williamson & Ruihan Fang</sup>"
     sections.append(title)
+
+    # ----- Method 1 ------
+
     method1title = "### Method 1: Store arrays in .npy files"
     sections.append(method1title)
     sections.append(linebreak)
-    method1body = "The first method was to follow what was shown in class and store the decks in numpy arrays. " \
-    "Each .npy file contains 10,000 decks, and each is " + str(npyfilesize_mb) + " MB.\n" \
-    "Table of results: "
+    method1body = "The first method was to follow what was shown in class and store the decks in numpy arrays, then save them as .npy files." \
+    " Each .npy file contains 10,000 decks, and each is " + str(npyfilesize_mb) + " MB." \
+    "\n\nTable of results: "
     sections.append(method1body)
     # Method to generate table
-    method1table = generate_table('npy')
+    method1table = generate_table(os.path.join("testing", "outputs", "npy_compiled_output.csv"))
     sections.append(method1table)
+
+    sections.append(linebreak)
+    # ----- Method 2 ------
+
+    method2title = "### Method 2: Store arrays in .bin files"
+    sections.append(method2title)
+    sections.append(linebreak)
+    method2body = "The first method was to take the numpy arrays and use the to_bytes method to store them in binary files" \
+    " Each .bin file contains 10,000 decks, and each is " + str(binfilesize_mb) + " MB." \
+    "\n\nTable of results: "
+    sections.append(method2body)
+    method2table = generate_table(os.path.join("testing", "outputs", "bin_compiled_output.csv"))
+    sections.append(method2table)
 
     for section in sections:
         mdstring += section+"\n"
     with open("DataGeneration.md", "w") as mdfile:
         mdfile.write(mdstring)
 
-def generate_table(method: str):
-    # Header row
-    output_file_name=method+"_compiled_output.csv" 
-    datapath = os.path.join("testing", "outputs", output_file_name)
+def generate_table(datapath: str):
+    
     with open(datapath, 'r') as file:
         csv_reader = csv.reader(file) 
         table_headers="|"
         table_divider = "|"
+
+        # Header row
         field_names = next(csv_reader)
         for field_name in field_names:
             table_headers +=" " + str(field_name) + " |"
             table_divider +="---|"
         
-        
+        # Actual data
         data_rows = "|"
-
         for row in csv_reader:
             for field in row:
                 data_rows += " " + str(field) + " |"
