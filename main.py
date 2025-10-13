43a6f3776009737060a11ec1a8455646b0496d3b
Carter Williamson <williamsoncarter27@gmail.com>
2025-10-13 17:18:39 -0400

    add heatmaps

--- main.py
+++ main.py
@@ -2,29 +2,52 @@ import os
 from src.deckGeneration import makeDecks
 from src.scoring import score_files
 from src.compileResult import compileResults
+from src.generateHeatmap import generateHeatmaps
 import time
 
 HALF_DECK = 26
 NUM_DECKS_FILE = 10000
-NUM_DECKS_TOTAL = 1000000
 PATH_TO_DATA = os.path.join('data')
 PATH_TO_SCORES = os.path.join('outputs', 'scores.csv')
 PATH_TO_OUTPUT = os.path.join('outputs', 'output.csv')
+PATH_TO_HEATMAPS = os.path.join('outputs')
 
-# Uncomment lines to run functions
+def getUserInput() -> int:
+    response = input("Hello! You can find the generated heatmaps under the outputs folder.\n" \
+    "How many decks would you like to generate and score? (0 to quit): ")
+    num_decks = int(response)
+    if num_decks != 0:
+        response = input(f"You are about to generate and score {num_decks:,} decks. Proceed? (y/n): ")
+        if response.lower() == 'y':
+            return num_decks
+    
+    exit(0)
 
-print("generating decks...")
-# Generate decks
-#makeDecks(half_deck=HALF_DECK, num_decks=NUM_DECKS_TOTAL, num_decks_file=NUM_DECKS_FILE, path_to_data=PATH_TO_DATA)
+def augment_data(n: int) -> None:
+    # Uncomment lines to run functions
 
-t1 = time.time()
-print("scoring...")
-# Score each deck, change filename to scored, update scores csv with scores
-#score_files(path_to_data=PATH_TO_DATA, path_to_scores=PATH_TO_SCORES)
-t2 = time.time()
+    print("generating decks...")
+    # Generate decks
+    makeDecks(half_deck=HALF_DECK, num_decks=n, num_decks_file=NUM_DECKS_FILE, path_to_data=PATH_TO_DATA)
 
-print("compiling results...")
-# Compile data into something readable
-compileResults(path_to_scores=PATH_TO_SCORES, path_to_output=PATH_TO_OUTPUT)
+    t1 = time.time()
+    print("scoring...")
+    # Score each deck, change filename to scored, update scores csv with scores
+    score_files(path_to_data=PATH_TO_DATA, path_to_scores=PATH_TO_SCORES)
+    t2 = time.time()
 
-print(f"Total time to score {NUM_DECKS_TOTAL} decks: {t2-t1:.2f} seconds")
+    print("compiling results...")
+    # Compile data into something readable
+    compileResults(path_to_scores=PATH_TO_SCORES, path_to_output=PATH_TO_OUTPUT)
+
+    print("generating heatmaps...")
+    # generate heatmap of results
+    generateHeatmaps(path_to_data=PATH_TO_OUTPUT, path_to_heatmaps=PATH_TO_HEATMAPS)
+
+    print(f"Total time to score {n:,} decks: {t2-t1:.2f} seconds")
+
+if __name__ == "__main__":
+    n = getUserInput()
+    augment_data(n)
+
+
---
+++ src/generateHeatmap.py
@@ -0,0 +1,63 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import os
+
+def retrieve_data(path_to_data: str) -> tuple[pd.DataFrame, pd.DataFrame]:
+    data = pd.read_csv(path_to_data)
+    trick_data = data[['p1_choice', 'p2_choice', 'p1_win%_tricks','tricks_tie%', 'games_total']]
+    card_data = data[['p1_choice', 'p2_choice', 'p1_win%_cards','cards_tie%', 'games_total']]
+    return trick_data, card_data
+
+def format_data(title: str, data: pd.DataFrame) -> pd.DataFrame:
+    # Input: Full data, with the title either tricks or cards
+    # Output: only the data related to the given title
+    title = title.lower()
+    refined_data = data[['p1_choice', 'p2_choice', 'games_total']]
+    winperc = data[f"p1_win%_{title}"].apply(lambda x: x[:-4]) # Sheer off decimal and %
+    tieperc = data[f"{title}_tie%"].apply(lambda x: "("+x[:-4]+")") # Add () around value, sheer off decimal and %
+    refined_data.insert(loc=0, column="winperc", value=winperc.astype(float))
+    refined_data.insert(loc=0, column="win_tie_perc", value=winperc+tieperc)
+    return refined_data
+
+def build_heatmap(title: str, data: pd.DataFrame, save_path: str) -> None:
+    data = format_data(title, data) # One could argue if this should be called before calling build_heatmap
+    games_total = data["games_total"][0]
+
+    # Begin charting
+    plt.figure(figsize = [8, 8])
+    
+    ax = sns.heatmap(data=data.pivot(index="p2_choice", columns="p1_choice", values="winperc"), 
+                annot=data.pivot(index="p2_choice", columns="p1_choice", values="win_tie_perc"), fmt="s",
+                cmap="Blues", cbar=False, linewidths=0.5)
+    plt.title(f"My Chance of Win(Draw)\nBy {title}\nN={games_total:,}", pad=10)
+    plt.xlabel("My Choice")
+    plt.ylabel("Opponent Choice")
+    plt.yticks(rotation=0)
+
+    # Begin box drawing
+
+    # First draw gray boxes over the diagonal
+    # Then draw boxes around each "best choice"
+    # for each choice, find max value location and add a border to it
+    unique_choice = data['p2_choice'].unique()
+    unique_choice.sort()
+    for i in range(len(unique_choice)):
+
+        # Make diagonals gray
+        ax.add_patch(plt.Rectangle((i, i), 1, 1, fill=True, facecolor='darkgray', edgecolor='white', lw=0.5))
+
+        # Find which p1_choice has the highest win% for each p2_choice
+        best_p1_choice = data.iloc[data[data['p2_choice'] == unique_choice[i]]['winperc'].idxmax()]['p1_choice']
+        # Draw border at the location of the best p1_choice for the given p2_choice
+        ax.add_patch(plt.Rectangle((np.where(unique_choice == best_p1_choice)[0][0], i), 1, 1, fill=False, edgecolor='black', lw=2))
+    
+    # save figure
+    plt.savefig(os.path.join(save_path, f'By{title}.svg'), bbox_inches = 'tight', facecolor = 'white')
+
+def generateHeatmaps(path_to_data: str, path_to_heatmaps: str) -> None:
+    trick_data, card_data = retrieve_data(path_to_data=path_to_data)
+    build_heatmap("Tricks", trick_data, path_to_heatmaps)
+    build_heatmap("Cards", card_data, path_to_heatmaps)
+    
